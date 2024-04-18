9399e182e29bf4dfa4e15be49a76d56a55aaccd2
Yers <eabuov4@gmail.com>
2024-04-18 22:32:03 +0500

    ORM2

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q, F
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
@@ -18,8 +19,21 @@ def say_hello(request):
     # queryset = Product.objects.filter(title__startswith='c') # start
     # queryset = Product.objects.filter(title__endswith='e') # end
     # queryset = Product.objects.filter(last_update__year=2021) # date
-    queryset = Product.objects.filter(description__isnull=True) # checking for null 
-    
-    
-    
+    # queryset = Product.objects.filter(description__isnull=True) # checking for null
+    # complex queries 
+    # queryset = Product.objects.filter(inventory__lt=10,unit_price__lt=20) 
+    # queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
+    # queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    # queryset = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20)) # ~ == not
+    # reference fields using f objects
+    # queryset = Product.objects.filter(inventory=F('unit_price'))
+    # queryset = Product.objects.filter(inventory=F('collection__id'))
+    # sorting data
+    # queryset = Product.objects.order_by('unit_price', '-title').reverse()
+    # queryset = Product.objects.filter(collection__id=1).order_by('unit_price')
+    # product = Product.objects.order_by('unit_price')[0]
+    # product = Product.objects.earlies('unit_price')
+    # product = Product.objects.latest('unit_price')
+    # limiting results
+    queryset = Product.objects.all()[:5]
     return render(request, 'hello.html', { 'name': 'Yers', 'products': list(queryset)})
