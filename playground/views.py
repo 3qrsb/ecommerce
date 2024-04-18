379eecc90751176f750337dc8f226fed37278196
Yers <eabuov4@gmail.com>
2024-04-18 23:04:08 +0500

    ORM3

--- playground/views.py
+++ playground/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.db.models import Q, F
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product, OrderItem
 
 
 
@@ -20,20 +20,43 @@ def say_hello(request):
     # queryset = Product.objects.filter(title__endswith='e') # end
     # queryset = Product.objects.filter(last_update__year=2021) # date
     # queryset = Product.objects.filter(description__isnull=True) # checking for null
+    
     # complex queries 
     # queryset = Product.objects.filter(inventory__lt=10,unit_price__lt=20) 
     # queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
     # queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
     # queryset = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20)) # ~ == not
+    
     # reference fields using f objects
     # queryset = Product.objects.filter(inventory=F('unit_price'))
     # queryset = Product.objects.filter(inventory=F('collection__id'))
+    
     # sorting data
     # queryset = Product.objects.order_by('unit_price', '-title').reverse()
     # queryset = Product.objects.filter(collection__id=1).order_by('unit_price')
     # product = Product.objects.order_by('unit_price')[0]
     # product = Product.objects.earlies('unit_price')
     # product = Product.objects.latest('unit_price')
+    
     # limiting results
     # queryset = Product.objects.all()[:5]
+    
+    # selecting specific fields
+    # queryset = Product.objects.values('id', 'title')
+    # queryset = Product.objects.values('id', 'title', 'collection__title') # access related fields with underscore, return dictionary
+    # queryset = Product.objects.values_list('id', 'title', 'collection__title') # returns tuple
+    # queryset = OrderItem.objects.values('product__id').distinct() # for removing duplicates
+    
+    # SELECT PRODUCTS THAT HAVE BEEN ORDERED AND SORT THEM BY TITLE
+    # queryset = Product.objects.filter(id__in=OrderItem.objects.values('product__id').distinct()).order_by('title')
+    
+    # deferring fields
+    # queryset = Product.objects.only('id', 'title') # returns instances of the product class
+    # queryset = Product.objects.defer('description')
+    
+    # selecting related objects
+    # select_related (1) other end of the relationship has one instance
+    # prefetch_related (n) has many objects
+    # queryset = Product.objects.select_related('collection__someOtherField').all()
+    queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()
     return render(request, 'hello.html', { 'name': 'Yers', 'products': list(queryset)})
