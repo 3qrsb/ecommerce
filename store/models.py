ac10bee81e34f9d74a9509197ed838377aa3ef7a
Yers <eabuov4@gmail.com>
2024-04-29 21:47:50 +0500

    saving objects

--- store/models.py
+++ store/models.py
@@ -8,17 +8,24 @@ class Promotion(models.Model):
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
-    # def __str__(self) -> str:
-    #     return self.title
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
 
 class Product(models.Model):
     title = models.CharField(max_length=255) #varchar 255
-    description = models.TextField()
+    slug = models.SlugField()
+    description = models.TextField(null=True, blank=True)
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(Promotion)
+    promotions = models.ManyToManyField(Promotion, blank=True)
+    
+    def __str__(self) -> str:
+        return self.title
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
--- store/serializers.py
+++ store/serializers.py
@@ -10,7 +10,7 @@ class CollectionSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
         # fields = '__all__'  bad practise
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     
@@ -18,7 +18,26 @@ class ProductSerializer(serializers.ModelSerializer):
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
     
+    def create(self, validated_data):
+        product = Product(**validated_data)
+        product.other = 1
+        product.save()
+        return product
+    
+    def update(self, instance, validated_data):
+        instance.unit_price = validated_data.get('unit_price')
+        instance.save()
+        return instance
+    
     # def validate(self, data):
     #     if data['password'] != data['confirm_password']:
     #         return serializers.ValidationError('Passwords do not match')
     #     return data
+    
+    {
+    "title": "a",
+    "slug": "a",
+    "unit_price": 1,
+    "collection": 1,
+    "inventory": 1
+    }
